84cddef3622df56f85e78480f9e30bc0f47e3f53
11ai <124818466+11ai@users.noreply.github.com>
2024-01-27 11:50:52 +0200

    edited on Sat Jan 27 2024 11:50:35 GMT+0200 (Eastern European Standard Time)

--- frontend/src/data.ts
+++ frontend/src/data.ts
@@ -1,3 +1,3 @@
 
-let data: any = {"defaultData":{"Books":[{"Title":"Mystery of the Emerald Sea","Author":"John Doe","Isbn":"123-456789123","Copiessold":15000,"Issuedate":"2000-05-23","Genre":"Mystery","Rating":4.5},{"Title":"The Lost World","Author":"Jane Doe","Isbn":"234-567891234","Copiessold":9000,"Issuedate":"2005-03-10","Genre":"Adventure","Rating":4},{"Title":"The Final Answer","Author":"Rose Tyler","Isbn":"345-678912345","Copiessold":20000,"Issuedate":"2010-07-06","Genre":"Science Fiction","Rating":4.8}]}};
+let data: any = {"defaultData":{"":[{"":4.3},{"":4.2},{"":3.9}]}};
 export default data;
