0dfffe3bb4d195807dcb6071a6208d33b0a058c2
11ai <124818466+11ai@users.noreply.github.com>
2024-01-27 12:48:54 +0200

    edited on Sat Jan 27 2024 12:48:29 GMT+0200 (Eastern European Standard Time)

--- backend/src/Database.ts
+++ backend/src/Database.ts
@@ -3,7 +3,7 @@ import { DataSource } from "typeorm";
 import { DBConfiguration } from "./Configuration";
 import { SettingsEntity } from "./db/Settings.entity";
 //autogenerate imports based on resources
-import { Entity } from "./db/.entity";
+import { BooksEntity } from "./db/Books.entity";
 
 export class Database {
   static dbConfiguration: DBConfiguration;
@@ -14,7 +14,7 @@ export class Database {
     let dbConfig: any = dbConfiguration as any;
     //Autogenerate entities array from resource names
 
-    dbConfig.entities = [SettingsEntity, Entity];
+    dbConfig.entities = [SettingsEntity, BooksEntity];
     Database.ds = new DataSource(dbConfig);
     await Database.ds.initialize();
 
@@ -24,14 +24,14 @@ export class Database {
     await Database.Seed();
   }
   static async Seed() {
-    let data: any = {"":[{"":"Science Fiction"},{"":"Romance"},{"":"Mystery"}]};
+    let data: any = {"Books":[{"Title":"Book 1","Author":"Author 1","ISBN":"ISBN 1","CopiesSold":100,"IssueDate":"2022-01-01","Genre":"Fiction"},{"Title":"Book 2","Author":"Author 2","ISBN":"ISBN 2","CopiesSold":200,"IssueDate":"2022-02-01","Genre":"Non-Fiction"},{"Title":"Book 3","Author":"Author 3","ISBN":"ISBN 3","CopiesSold":300,"IssueDate":"2022-03-01","Genre":"Fantasy"}]};
     //Autogenerate multiple such calls ie for each resource and its data object
     let isSeeded = await this.IsSeeded();
     //if (!isSeeded) {
     //forcing app recreation
     if (true){
       console.log('   Seeding database...');
-      await this.SeedResource("Entity", data.); 
+      await this.SeedResource("BooksEntity", data.Books); 
       await this.SeedResource("SettingsEntity", {
         settingname: "isSeeded",
         settingvalue: "true",
