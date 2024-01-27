6e64eba2ca8e031ed2ea64fb0b9cda8595f6521d
11ai <124818466+11ai@users.noreply.github.com>
2024-01-27 11:38:11 +0200

    edited on Sat Jan 27 2024 11:37:58 GMT+0200 (Eastern European Standard Time)

--- frontend/src/data.ts
+++ frontend/src/data.ts
@@ -1,3 +1,3 @@
 
-let data: any = {"defaultData":{"Books":[{"bookTitle":"A book title 1","releaseDate":"01-01-2000","numberOfBooksSold":10000,"author":"Author 1","numberOfEditions":3},{"bookTitle":"A book title 2","releaseDate":"04-06-2005","numberOfBooksSold":50000,"author":"Author 2","numberOfEditions":5},{"bookTitle":"A book title 3","releaseDate":"10-07-2010","numberOfBooksSold":76000,"author":"Author 3","numberOfEditions":7}],"Authors":[{"authorName":"Author 1","dateOfBirth":"01-01-1970","sex":"Male","countryOfBirth":"Country 1"},{"authorName":"Author 2","dateOfBirth":"02-02-1975","sex":"Female","countryOfBirth":"Country 2"},{"authorName":"Author 3","dateOfBirth":"03-03-1980","sex":"Male","countryOfBirth":"Country 3"}]}};
+let data: any = {"defaultData":{"Books":[{},{},{}]}};
 export default data;
