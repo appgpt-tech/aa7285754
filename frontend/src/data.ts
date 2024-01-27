2c74406426ce114236aff7c68771252a3a497354
11ai <124818466+11ai@users.noreply.github.com>
2024-01-27 12:48:40 +0200

    edited on Sat Jan 27 2024 12:48:29 GMT+0200 (Eastern European Standard Time)

--- frontend/src/data.ts
+++ frontend/src/data.ts
@@ -1,3 +1,3 @@
 
-let data: any = {"defaultData":{"":[{"":"Science Fiction"},{"":"Romance"},{"":"Mystery"}]}};
+let data: any = {"defaultData":{"Books":[{"Title":"Book 1","Author":"Author 1","ISBN":"ISBN 1","CopiesSold":100,"IssueDate":"2022-01-01","Genre":"Fiction"},{"Title":"Book 2","Author":"Author 2","ISBN":"ISBN 2","CopiesSold":200,"IssueDate":"2022-02-01","Genre":"Non-Fiction"},{"Title":"Book 3","Author":"Author 3","ISBN":"ISBN 3","CopiesSold":300,"IssueDate":"2022-03-01","Genre":"Fantasy"}]}};
 export default data;
