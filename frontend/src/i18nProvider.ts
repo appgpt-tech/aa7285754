e722c89defc94cf95b77ca0371ba1f24f1482968
11ai <124818466+11ai@users.noreply.github.com>
2024-01-27 03:32:42 +0200

    edited on Sat Jan 27 2024 01:32:29 GMT+0000 (Coordinated Universal Time)

--- frontend/src/i18nProvider.ts
+++ frontend/src/i18nProvider.ts
@@ -3,7 +3,7 @@
     import polyglotI18nProvider from "ra-i18n-polyglot";
     import enOriginal from 'ra-language-english';
 
-    const enResources = { resources: {"Books":{"name":"Books","fields":{"bookTitle":"bookTitle","releaseDate":"releaseDate","numberOfBooksSold":"numberOfBooksSold","author":"author","numberOfEditions":"numberOfEditions","id":"id"}},"Authors":{"name":"Authors","fields":{"authorName":"authorName","dateOfBirth":"dateOfBirth","gender":"gender","countryOfBirth":"countryOfBirth","id":"id"}}}};
+    const enResources = { resources: {"Books":{"name":"Books","fields":{"bookTitle":"bookTitle","releaseDate":"releaseDate","numberOfBooksSold":"numberOfBooksSold","author":"author","numberOfEditions":"numberOfEditions","id":"id"}},"Authors":{"name":"Authors","fields":{"authorName":"authorName","dateOfBirth":"dateOfBirth","sex":"sex","countryOfBirth":"countryOfBirth","id":"id"}}}};
 
 
     const en = mergeTranslations(enOriginal,enResources);
