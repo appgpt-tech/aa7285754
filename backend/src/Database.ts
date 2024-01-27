7ed82737af9b7949f677339b9a2af1dee4ec7527
11ai <124818466+11ai@users.noreply.github.com>
2024-01-27 11:50:59 +0200

    edited on Sat Jan 27 2024 11:50:35 GMT+0200 (Eastern European Standard Time)

--- backend/src/Database.ts
+++ backend/src/Database.ts
@@ -3,7 +3,7 @@ import { DataSource } from "typeorm";
 import { DBConfiguration } from "./Configuration";
 import { SettingsEntity } from "./db/Settings.entity";
 //autogenerate imports based on resources
-import { BooksEntity } from "./db/Books.entity";
+import { Entity } from "./db/.entity";
 
 export class Database {
   static dbConfiguration: DBConfiguration;
@@ -14,7 +14,7 @@ export class Database {
     let dbConfig: any = dbConfiguration as any;
     //Autogenerate entities array from resource names
 
-    dbConfig.entities = [SettingsEntity, BooksEntity];
+    dbConfig.entities = [SettingsEntity, Entity];
     Database.ds = new DataSource(dbConfig);
     await Database.ds.initialize();
 
@@ -24,14 +24,14 @@ export class Database {
     await Database.Seed();
   }
   static async Seed() {
-    let data: any = {"Books":[{"Title":"Mystery of the Emerald Sea","Author":"John Doe","Isbn":"123-456789123","Copiessold":15000,"Issuedate":"2000-05-23","Genre":"Mystery","Rating":4.5},{"Title":"The Lost World","Author":"Jane Doe","Isbn":"234-567891234","Copiessold":9000,"Issuedate":"2005-03-10","Genre":"Adventure","Rating":4},{"Title":"The Final Answer","Author":"Rose Tyler","Isbn":"345-678912345","Copiessold":20000,"Issuedate":"2010-07-06","Genre":"Science Fiction","Rating":4.8}]};
+    let data: any = {"":[{"":4.3},{"":4.2},{"":3.9}]};
     //Autogenerate multiple such calls ie for each resource and its data object
     let isSeeded = await this.IsSeeded();
     //if (!isSeeded) {
     //forcing app recreation
     if (true){
       console.log('   Seeding database...');
-      await this.SeedResource("BooksEntity", data.Books); 
+      await this.SeedResource("Entity", data.); 
       await this.SeedResource("SettingsEntity", {
         settingname: "isSeeded",
         settingvalue: "true",
