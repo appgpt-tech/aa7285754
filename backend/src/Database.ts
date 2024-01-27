fbe8e412e21244d08f33e8d3975139a151bfb6d5
11ai <124818466+11ai@users.noreply.github.com>
2024-01-27 03:21:43 +0200

    created on Sat Jan 27 2024 01:21:16 GMT+0000 (Coordinated Universal Time)

---
+++ backend/src/Database.ts
@@ -0,0 +1,66 @@
+//Class to create tables and seed new database
+import { DataSource } from "typeorm";
+import { DBConfiguration } from "./Configuration";
+import { SettingsEntity } from "./db/Settings.entity";
+//autogenerate imports based on resources
+import { BooksEntity } from "./db/Books.entity";
+import { AuthorsEntity } from "./db/Authors.entity";
+
+export class Database {
+  static dbConfiguration: DBConfiguration;
+  public static ds: DataSource;
+
+  static async Initialize(dbConfiguration: DBConfiguration) {
+    Database.dbConfiguration = dbConfiguration;
+    let dbConfig: any = dbConfiguration as any;
+    //Autogenerate entities array from resource names
+
+    dbConfig.entities = [SettingsEntity, BooksEntity, AuthorsEntity];
+    Database.ds = new DataSource(dbConfig);
+    await Database.ds.initialize();
+
+    //TODO: Drop all tables
+
+
+    await Database.Seed();
+  }
+  static async Seed() {
+    let data: any = {"Books":[{"bookTitle":"Book 1","releaseDate":"2001-01-01","numberOfBooksSold":5000,"author":"Author 1","numberOfEditions":1},{"bookTitle":"Book 2","releaseDate":"2002-02-02","numberOfBooksSold":10000,"author":"Author 2","numberOfEditions":2},{"bookTitle":"Book 3","releaseDate":"2003-03-03","numberOfBooksSold":1000,"author":"Author 3","numberOfEditions":3}],"Authors":[{"authorName":"Author 1","dateOfBirth":"1970-01-01","gender":"Male","countryOfBirth":"USA"},{"authorName":"Author 2","dateOfBirth":"1980-01-01","gender":"Female","countryOfBirth":"UK"},{"authorName":"Author 3","dateOfBirth":"1990-01-01","gender":"Non-binary","countryOfBirth":"Canada"}]};
+    //Autogenerate multiple such calls ie for each resource and its data object
+    let isSeeded = await this.IsSeeded();
+    //if (!isSeeded) {
+    //forcing app recreation
+    if (true){
+      console.log('   Seeding database...');
+      await this.SeedResource("BooksEntity", data.Books);
+await this.SeedResource("AuthorsEntity", data.Authors); 
+      await this.SeedResource("SettingsEntity", {
+        settingname: "isSeeded",
+        settingvalue: "true",
+      });
+    }else{
+      console.log('   Database seeded already!');
+    }
+  }
+  static async IsSeeded() {
+    const repo = Database.ds.getRepository("SettingsEntity");
+    let rec: any = await repo.findOne({
+      select: {
+        settingname: true,
+        settingvalue: true,
+      },
+      where: {
+        settingname: "isSeeded",
+      },
+    });
+    if (rec && rec.settingvalue) return true;
+    return false;
+  }
+  static async SeedResource(resourceName: any, resourceData: any) {
+    const repo = Database.ds.getRepository(resourceName);
+    //await repo.clear();
+    console.log('   Seeding table '+resourceName);
+    await repo.upsert(resourceData, ["id"]);
+  }
+}
+
