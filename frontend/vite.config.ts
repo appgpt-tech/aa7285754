dfc778c563b72fa43e8087ab62f09a8f3d82c3b3
11ai <124818466+11ai@users.noreply.github.com>
2024-01-27 03:21:23 +0200

    created on Sat Jan 27 2024 01:21:16 GMT+0000 (Coordinated Universal Time)

---
+++ frontend/vite.config.ts
@@ -0,0 +1,17 @@
+
+  import { defineConfig } from 'vite'
+  import react from '@vitejs/plugin-react'
+  // https://vitejs.dev/config/
+  export default defineConfig({
+    plugins: [react()],
+    base: '/aa7285754/',
+    build: {
+      outDir: '.output/aa7285754',
+      emptyOutDir: true
+    },
+    server: {
+      host: true,
+      strictPort: true,
+      port: 5173
+    }
+  })
