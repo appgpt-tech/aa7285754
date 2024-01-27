8cb02b3a9c69d50059baa05f9364c5ea86f0a300
11ai <124818466+11ai@users.noreply.github.com>
2024-01-27 03:32:55 +0200

    edited on Sat Jan 27 2024 01:32:29 GMT+0000 (Coordinated Universal Time)

--- backend/src/Database.ts
+++ backend/src/Database.ts
@@ -25,7 +25,7 @@ export class Database {
     await Database.Seed();
   }
   static async Seed() {
-    let data: any = {"Books":[{"bookTitle":"Book 1","releaseDate":"2001-01-01","numberOfBooksSold":5000,"author":"Author 1","numberOfEditions":1},{"bookTitle":"Book 2","releaseDate":"2002-02-02","numberOfBooksSold":10000,"author":"Author 2","numberOfEditions":2},{"bookTitle":"Book 3","releaseDate":"2003-03-03","numberOfBooksSold":1000,"author":"Author 3","numberOfEditions":3}],"Authors":[{"authorName":"Author 1","dateOfBirth":"1970-01-01","gender":"Male","countryOfBirth":"USA"},{"authorName":"Author 2","dateOfBirth":"1980-01-01","gender":"Female","countryOfBirth":"UK"},{"authorName":"Author 3","dateOfBirth":"1990-01-01","gender":"Non-binary","countryOfBirth":"Canada"}]};
+    let data: any = {"Books":[{"bookTitle":"A book title 1","releaseDate":"01-01-2000","numberOfBooksSold":10000,"author":"Author 1","numberOfEditions":3},{"bookTitle":"A book title 2","releaseDate":"04-06-2005","numberOfBooksSold":50000,"author":"Author 2","numberOfEditions":5},{"bookTitle":"A book title 3","releaseDate":"10-07-2010","numberOfBooksSold":76000,"author":"Author 3","numberOfEditions":7}],"Authors":[{"authorName":"Author 1","dateOfBirth":"01-01-1970","sex":"Male","countryOfBirth":"Country 1"},{"authorName":"Author 2","dateOfBirth":"02-02-1975","sex":"Female","countryOfBirth":"Country 2"},{"authorName":"Author 3","dateOfBirth":"03-03-1980","sex":"Male","countryOfBirth":"Country 3"}]};
     //Autogenerate multiple such calls ie for each resource and its data object
     let isSeeded = await this.IsSeeded();
     //if (!isSeeded) {
