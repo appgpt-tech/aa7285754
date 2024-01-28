9e783da897c59dd3ce2a69d26fb32cd634a1c85f
11ai <124818466+11ai@users.noreply.github.com>
2024-01-28 22:49:34 +0200

    edited on Sun Jan 28 2024 22:49:24 GMT+0200 (Eastern European Standard Time)

--- frontend/src/data.ts
+++ frontend/src/data.ts
@@ -1,3 +1,3 @@
 
-let data: any = {"defaultData":{"Books":[{"Title":"Fake Book Title 1","Author":"Fake Author 1","ISBN":"1234567890","CopiesSold":10000,"IssueDate":"2001-01-01","Genre":"Fiction"},{"Title":"Fake Book Title 2","Author":"Fake Author 2","ISBN":"0987654321","CopiesSold":5000,"IssueDate":"2002-02-02","Genre":"Non-fiction"},{"Title":"Fake Book Title 3","Author":"Fake Author 3","ISBN":"1112131415","CopiesSold":2000,"IssueDate":"2003-03-03","Genre":"Sci-fi"}]}};
+let data: any = {"defaultData":{"Books":[{"Title":"Book 1","Author":"Author 1","ISBN":"1234567890","CopiesSold":10000,"IssueDate":"2020-01-01","Genre":"Fiction"},{"Title":"Book 2","Author":"Author 2","ISBN":"2345678901","CopiesSold":20000,"IssueDate":"2019-01-01","Genre":"Non-fiction"},{"Title":"Book 3","Author":"Author 3","ISBN":"3456789012","CopiesSold":30000,"IssueDate":"2021-01-01","Genre":"Fantasy"}]}};
 export default data;
