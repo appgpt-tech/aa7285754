60bf4cf2e3d2a6ad5b8b4ab5a0a142aa9798e1d3
11ai <124818466+11ai@users.noreply.github.com>
2024-02-02 18:30:10 +0200

    edited on Fri Feb 02 2024 18:29:59 GMT+0200 (Eastern European Standard Time)

--- frontend/src/data.ts
+++ frontend/src/data.ts
@@ -1,3 +1,3 @@
 
-let data: any = {"defaultData":{"Books":[],"Authors":[],"Genres":[]}};
+let data: any = {"defaultData":{"Projects":[{"projectName":"Project 1","description":"First Project"},{"projectName":"Project 2","description":"Second Project"},{"projectName":"Project 3","description":"Third Project"}],"Tasks":[{"taskName":"Task 1","description":"First Task"},{"taskName":"Task 2","description":"Second Task"},{"taskName":"Task 3","description":"Third Task"}],"Assignees":[{"assigneeName":"Assignee 1","role":"Role 1"},{"assigneeName":"Assignee 2","role":"Role 2"},{"assigneeName":"Assignee 3","role":"Role 3"}]}};
 export default data;
