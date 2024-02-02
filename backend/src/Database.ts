84214d6f0f31b5adb8fd1ff298246117b50e92eb
11ai <124818466+11ai@users.noreply.github.com>
2024-02-02 18:30:16 +0200

    edited on Fri Feb 02 2024 18:29:59 GMT+0200 (Eastern European Standard Time)

--- backend/src/Database.ts
+++ backend/src/Database.ts
@@ -3,9 +3,9 @@ import { DataSource } from "typeorm";
 import { DBConfiguration } from "./Configuration";
 import { SettingsEntity } from "./db/Settings.entity";
 //autogenerate imports based on resources
-import { BooksEntity } from "./db/Books.entity";
-import { AuthorsEntity } from "./db/Authors.entity";
-import { GenresEntity } from "./db/Genres.entity";
+import { ProjectsEntity } from "./db/Projects.entity";
+import { TasksEntity } from "./db/Tasks.entity";
+import { AssigneesEntity } from "./db/Assignees.entity";
 
 export class Database {
   static dbConfiguration: DBConfiguration;
@@ -16,7 +16,7 @@ export class Database {
     let dbConfig: any = dbConfiguration as any;
     //Autogenerate entities array from resource names
 
-    dbConfig.entities = [SettingsEntity, BooksEntity, AuthorsEntity, GenresEntity];
+    dbConfig.entities = [SettingsEntity, ProjectsEntity, TasksEntity, AssigneesEntity];
     Database.ds = new DataSource(dbConfig);
     await Database.ds.initialize();
 
@@ -26,16 +26,16 @@ export class Database {
     await Database.Seed();
   }
   static async Seed() {
-    let data: any = {"Books":[],"Authors":[],"Genres":[]};
+    let data: any = {"Projects":[{"projectName":"Project 1","description":"First Project"},{"projectName":"Project 2","description":"Second Project"},{"projectName":"Project 3","description":"Third Project"}],"Tasks":[{"taskName":"Task 1","description":"First Task"},{"taskName":"Task 2","description":"Second Task"},{"taskName":"Task 3","description":"Third Task"}],"Assignees":[{"assigneeName":"Assignee 1","role":"Role 1"},{"assigneeName":"Assignee 2","role":"Role 2"},{"assigneeName":"Assignee 3","role":"Role 3"}]};
     //Autogenerate multiple such calls ie for each resource and its data object
     let isSeeded = await this.IsSeeded();
     //if (!isSeeded) {
     //forcing app recreation
     if (true){
       console.log('   Seeding database...');
-      await this.SeedResource("BooksEntity", data.Books);
-await this.SeedResource("AuthorsEntity", data.Authors);
-await this.SeedResource("GenresEntity", data.Genres); 
+      await this.SeedResource("ProjectsEntity", data.Projects);
+await this.SeedResource("TasksEntity", data.Tasks);
+await this.SeedResource("AssigneesEntity", data.Assignees); 
       await this.SeedResource("SettingsEntity", {
         settingname: "isSeeded",
         settingvalue: "true",
