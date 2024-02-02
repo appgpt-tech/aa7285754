ea53435c69f51e56b1f65059f98249f0c044f792
11ai <124818466+11ai@users.noreply.github.com>
2024-02-02 18:10:21 +0200

    edited on Fri Feb 02 2024 18:10:04 GMT+0200 (Eastern European Standard Time)

--- frontend/src/i18nProvider.ts
+++ frontend/src/i18nProvider.ts
@@ -4,8 +4,8 @@
     import enOriginal from 'ra-language-english';
 import frOriginal from 'ra-language-french';
 
-    const enResources = { resources: {"Books":{"name":"Books","fields":{"title":"Title","author":"Author","genre":"Genre","bookCover":"Book Cover","id":"id"}},"Authors":{"name":"Authors","fields":{"name":"Name","books":"Books","id":"id"}},"Genres":{"name":"Genres","fields":{"category":"Category","id":"id"}}}};
-const frResources = { resources: {"Books":{"name":"Livres","fields":{"title":"Titre","author":"Auteur","genre":"Genre","bookCover":"Couverture du livre","id":"id"}},"Authors":{"name":"Auteurs","fields":{"name":"Nom","books":"Livres","id":"id"}},"Genres":{"name":"Genres","fields":{"category":"Catégorie","id":"id"}}}};
+    const enResources = { resources: {"Books":{"name":"Books","fields":{"title":"Book Title","author":"Author","genre":"Genre","bookCover":"Book Cover","id":"id"}},"Authors":{"name":"Authors","fields":{"name":"Name","books":"Books","id":"id"}},"Genres":{"name":"Genres","fields":{"category":"Category","id":"id"}}}};
+const frResources = { resources: {"Books":{"name":"Livres","fields":{"title":"Titre de livre","author":"Auteur","genre":"Genre","bookCover":"Couverture de livre","id":"id"}},"Authors":{"name":"Auteurs","fields":{"name":"Nom","books":"Livres","id":"id"}},"Genres":{"name":"Genres","fields":{"category":"Categorie","id":"id"}}}};
 
 
     const en = mergeTranslations(enOriginal,enResources);
