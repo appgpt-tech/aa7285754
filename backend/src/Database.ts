301ef1c0c54dab968db90c5e14edb42fb0c016c5
11ai <124818466+11ai@users.noreply.github.com>
2024-02-02 17:50:52 +0200

    edited on Fri Feb 02 2024 17:50:27 GMT+0200 (Eastern European Standard Time)

--- backend/src/Database.ts
+++ backend/src/Database.ts
@@ -26,7 +26,7 @@ export class Database {
     await Database.Seed();
   }
   static async Seed() {
-    let data: any = {"Books":[{"title":"Generated title 1","authorName":"Generated author name 1","genre":"Generated genre 1","bookCover":"Generated book cover URL 1"},{"title":"Generated title 2","authorName":"Generated author name 2","genre":"Generated genre 2","bookCover":"Generated book cover URL 2"},{"title":"Generated title 3","authorName":"Generated author name 3","genre":"Generated genre 3","bookCover":"Generated book cover URL 3"}],"Authors":[{"authorName":"Generated author name 1","books":"Generated book title 1"},{"authorName":"Generated author name 2","books":"Generated book title 2"},{"authorName":"Generated author name 3","books":"Generated book title 3"}],"Genres":[{"category":"Generated category 1"},{"category":"Generated category 2"},{"category":"Generated category 3"}]};
+    let data: any = {"Books":[],"Authors":[],"Genres":[]};
     //Autogenerate multiple such calls ie for each resource and its data object
     let isSeeded = await this.IsSeeded();
     //if (!isSeeded) {
