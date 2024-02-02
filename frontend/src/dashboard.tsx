96958f11ce870270132e5656789dd90bdd91603c
11ai <124818466+11ai@users.noreply.github.com>
2024-02-02 17:34:19 +0200

    edited on Fri Feb 02 2024 17:34:09 GMT+0200 (Eastern European Standard Time)

--- frontend/src/dashboard.tsx
+++ frontend/src/dashboard.tsx
@@ -2,6 +2,6 @@ import { Card, CardContent, CardHeader } from "@mui/material";
     export const Dashboard = () => (
       <Card>
       <CardHeader title= "libby" />
-      <CardContent> my app is a book archive of sorts < /CardContent>
+      <CardContent> a book archive of sorts < /CardContent>
       </Card>
     );
