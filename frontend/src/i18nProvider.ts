2c464d1d4fbef69fc692110d218de15694f0c4d0
11ai <124818466+11ai@users.noreply.github.com>
2024-02-02 17:50:40 +0200

    edited on Fri Feb 02 2024 17:50:27 GMT+0200 (Eastern European Standard Time)

--- frontend/src/i18nProvider.ts
+++ frontend/src/i18nProvider.ts
@@ -2,16 +2,19 @@
     import { mergeTranslations } from "ra-core";
     import polyglotI18nProvider from "ra-i18n-polyglot";
     import enOriginal from 'ra-language-english';
+import frOriginal from 'ra-language-french';
 
-    const enResources = { resources: {"Books":{"name":"books","fields":{"title":"Book Title","authorName":"Author Name","genre":"Genre","bookCover":"Book Cover","id":"id"}},"Authors":{"name":"authors","fields":{"authorName":"Author Name","books":"Books","id":"id"}},"Genres":{"name":"genres","fields":{"category":"Category","id":"id"}}}};
+    const enResources = { resources: {"Books":{"name":"Books","fields":{"title":"Title","author":"Author","genre":"Genre","bookCover":"Book Cover","id":"id"}},"Authors":{"name":"Authors","fields":{"name":"Name","books":"Books","id":"id"}},"Genres":{"name":"Genres","fields":{"category":"Category","id":"id"}}}};
+const frResources = { resources: {"Books":{"name":"Livres","fields":{"title":"Titre","author":"Auteur","genre":"Genre","bookCover":"Couverture du livre","id":"id"}},"Authors":{"name":"Auteurs","fields":{"name":"Nom","books":"Livres","id":"id"}},"Genres":{"name":"Genres","fields":{"category":"Catégorie","id":"id"}}}};
 
 
     const en = mergeTranslations(enOriginal,enResources);
+const fr = mergeTranslations(frOriginal,frResources);
 
-    const translations = { en};
+    const translations = { en, fr};
     export const i18nProvider = polyglotI18nProvider(
       (locale) => translations[locale],
       "en", //default locale
-      [{"locale":"en","name":"English"}]
+      [{"locale":"en","name":"English"},{"locale":"fr","name":"Français"}]
     );
     
