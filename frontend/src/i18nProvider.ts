7b8446471bb7b8eec4a1f8ff64030da4727b568a
11ai <124818466+11ai@users.noreply.github.com>
2024-02-02 16:20:53 +0200

    edited on Fri Feb 02 2024 16:20:29 GMT+0200 (Eastern European Standard Time)

--- frontend/src/i18nProvider.ts
+++ frontend/src/i18nProvider.ts
@@ -2,16 +2,19 @@
     import { mergeTranslations } from "ra-core";
     import polyglotI18nProvider from "ra-i18n-polyglot";
     import enOriginal from 'ra-language-english';
+import frOriginal from 'ra-language-french';
 
     const enResources = { resources: {"Books":{"name":"Books","fields":{"title":"title","author":"author","genre":"genre","bookCover":"bookCover","id":"id"}},"Authors":{"name":"Authors","fields":{"name":"name","books":"books","id":"id"}},"Genres":{"name":"Genres","fields":{"category":"category","id":"id"}}}};
+const frResources = { resources: {"Books":{"name":"Livres","fields":{"title":"title","author":"author","genre":"genre","bookCover":"bookCover","id":"id"}},"Authors":{"name":"Auteurs","fields":{"name":"name","books":"books","id":"id"}},"Genres":{"name":"Genres","fields":{"category":"category","id":"id"}}}};
 
 
     const en = mergeTranslations(enOriginal,enResources);
+const fr = mergeTranslations(frOriginal,frResources);
 
-    const translations = { en};
+    const translations = { en, fr};
     export const i18nProvider = polyglotI18nProvider(
       (locale) => translations[locale],
       "en", //default locale
-      [{"locale":"en","name":"English"}]
+      [{"locale":"en","name":"English"},{"locale":"fr","name":"Français"}]
     );
     
