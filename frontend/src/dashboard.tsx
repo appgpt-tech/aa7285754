c28c92341cc31e4083852dc7a347c48d2b4dd7c9
11ai <124818466+11ai@users.noreply.github.com>
2024-02-05 21:19:34 +0200

    edited on Mon Feb 05 2024 21:19:08 GMT+0200 (Eastern European Standard Time)

--- frontend/src/dashboard.tsx
+++ frontend/src/dashboard.tsx
@@ -1,7 +1,7 @@
 import { Card, CardContent, CardHeader } from "@mui/material";
     export const Dashboard = () => (
       <Card>
-      <CardHeader title= "project mgmt" />
-      <CardContent> An application for managing various tasks and assignees across projects < /CardContent>
+      <CardHeader title= "Project Mgmt" />
+      <CardContent> An application for project management < /CardContent>
       </Card>
     );
