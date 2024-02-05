8b718a222fd94b32c609a4d45fff75516df8fdde
11ai <124818466+11ai@users.noreply.github.com>
2024-02-05 21:24:02 +0200

    edited on Mon Feb 05 2024 21:23:50 GMT+0200 (Eastern European Standard Time)

--- frontend/src/i18nProvider.ts
+++ frontend/src/i18nProvider.ts
@@ -4,8 +4,8 @@
     import enOriginal from 'ra-language-english';
 import frOriginal from 'ra-language-french';
 
-    const enResources = { resources: {"Projects":{"name":"Projects","fields":{"projectName":"Project Name","description":"Description","startDate":"Start Date","endDate":"End Date","status":"Status","id":"id"}},"Tasks":{"name":"Tasks","fields":{"taskName":"Task Name","description":"Description","dueDate":"Due Date","status":"Status","priority":"Priority","assigneeName":"Assignee Name","projectName":"Project Name","id":"id"}},"Assignees":{"name":"Assignees","fields":{"assigneeName":"Assignee Name","role":"Role","email":"Email","id":"id"}}}};
-const frResources = { resources: {"Projects":{"name":"Projets","fields":{"projectName":"Nom du Projet","description":"description","startDate":"Date de début","endDate":"Date de fin","status":"Statut","id":"id"}},"Tasks":{"name":"Tâches","fields":{"taskName":"Nom de la tâche","description":"Description","dueDate":"Date d'échéance","status":"Statut","priority":"Priorité","assigneeName":"Nom du responsable","projectName":"Nom du Projet","id":"id"}},"Assignees":{"name":"Les assignataires","fields":{"assigneeName":"Nom du responsable","role":"Rôle","email":"Email","id":"id"}}}};
+    const enResources = { resources: {"Projects":{"name":"Projects","fields":{"projectName":"Project Name","description":"Description","id":"id"}},"Tasks":{"name":"Tasks","fields":{"taskName":"Task Name","id":"id"}},"Assignees":{"name":"Assignees","fields":{"assigneeName":"Assignee Name","role":"Role","email":"Email","id":"id"}}}};
+const frResources = { resources: {"Projects":{"name":"Projets","fields":{"projectName":"Nom du Projet","description":"Description","id":"id"}},"Tasks":{"name":"Tâches","fields":{"taskName":"Nom de la Tâche","id":"id"}},"Assignees":{"name":"Attributaires","fields":{"assigneeName":"Nom de l'Attributaire","role":"Role","email":"Email","id":"id"}}}};
 
 
     const en = mergeTranslations(enOriginal,enResources);
