6ff400b74c2990ccfe67724d76eda9a2219755be
11ai <124818466+11ai@users.noreply.github.com>
2024-02-07 01:00:47 +0200

    created on Tue Feb 06 2024 23:00:18 GMT+0000 (Coordinated Universal Time)

---
+++ frontend/src/resources/ShoppingCart.tsx
@@ -0,0 +1,97 @@
+import {
+  Datagrid,
+  List,
+  EditButton,
+  Edit,
+  SimpleForm,
+  Create,
+  SelectColumnsButton,
+  DatagridConfigurable,
+  TopToolbar,
+  CreateButton,
+  ExportButton,
+  FilterButton,
+  //Field controls
+  BooleanField,
+  DateField,
+  EmailField,
+  ImageField,
+  NumberField,
+  ReferenceField,
+  TextField,
+  UrlField,
+  //Input controls
+  BooleanInput,
+  DateInput,
+  //EmailInput,
+  ImageInput,
+  NumberInput,
+  ReferenceInput,
+  TextInput,
+  //UrlInput,
+} from "react-admin";
+import { useRecordContext } from "react-admin";
+const ListActions = () => (
+    <TopToolbar>
+        <FilterButton />
+        <CreateButton />
+        <ExportButton />
+        <SelectColumnsButton />
+    </TopToolbar>
+);
+const ShoppingCartTitle = () => {
+  const record = useRecordContext();
+  return <span>ShoppingCart {record ? `"${ record.cartId }"` : ""}</span>;
+};
+
+export const ShoppingCartList = () => (
+      <List actions={<ListActions  />} filters={ResourceFilters} >
+        <DatagridConfigurable>
+          <TextField source="cartId" />
+<ReferenceField source="customerId" reference="Customers"  />
+<ReferenceField source="productId" reference="Products"  />
+<NumberField source="price" />
+<NumberField source="quantity" />
+<NumberField source="id" /><EditButton />
+
+        </DatagridConfigurable>
+      </List>
+      );
+
+export const ShoppingCartEdit = () => (
+                    <Edit title={<ShoppingCartTitle />}>
+                      <SimpleForm>
+                          <TextInput source="cartId"   />
+<ReferenceInput source="customerId"  reference="Customers"   />
+<ReferenceInput source="productId"  reference="Products"   />
+<NumberInput source="price"   />
+<NumberInput source="quantity"   />
+<NumberInput source="id"   disabled/>
+                      </SimpleForm>
+                    </Edit>
+                  );
+
+export const ShoppingCartCreate = () => (
+                                  <Create>
+                                    <SimpleForm>
+                                        <TextInput source="cartId"   />
+<ReferenceInput source="customerId"  reference="Customers"   />
+<ReferenceInput source="productId"  reference="Products"   />
+<NumberInput source="price"   />
+<NumberInput source="quantity"   />
+<NumberInput source="id"   disabled/>
+                                    </SimpleForm>
+                                  </Create>
+                                );
+
+const ResourceFilters = [
+      <TextInput source="q" label="Search" alwaysOn />,
+,
+<ReferenceInput source="customerId" label="customerId" reference="Customers"   alwaysOn/>,
+<ReferenceInput source="productId" label="productId" reference="Products"   alwaysOn/>,
+,
+,
+
+    ];
+
+
