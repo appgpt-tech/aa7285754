7178139cf906e6fc1309cf689d354f794326f288
11ai <124818466+11ai@users.noreply.github.com>
2024-02-07 01:04:31 +0200

    edited on Tue Feb 06 2024 23:04:19 GMT+0000 (Coordinated Universal Time)

--- frontend/src/dashboard.tsx
+++ frontend/src/dashboard.tsx
@@ -1,7 +1,7 @@
 import { Card, CardContent, CardHeader } from "@mui/material";
     export const Dashboard = () => (
       <Card>
-      <CardHeader title= "ShopSphere" />
-      <CardContent> My app is an ecommerce site to provide a seamless shopping experience for users worldwide. < /CardContent>
+      <CardHeader title= "EmpowerHR" />
+      <CardContent> My app is an HR application used by a company to streamline the management of employee information, recruitment, performance evaluations, leave requests, and payroll. < /CardContent>
       </Card>
     );
