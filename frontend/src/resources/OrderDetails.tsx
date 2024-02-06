36e27e7396b1db04871077941ef7d541b5061d66
11ai <124818466+11ai@users.noreply.github.com>
2024-02-07 01:00:37 +0200

    created on Tue Feb 06 2024 23:00:18 GMT+0000 (Coordinated Universal Time)

---
+++ frontend/src/resources/OrderDetails.tsx
@@ -0,0 +1,93 @@
+import {
+  Datagrid,
+  List,
+  EditButton,
+  Edit,
+  SimpleForm,
+  Create,
+  SelectColumnsButton,
+  DatagridConfigurable,
+  TopToolbar,
+  CreateButton,
+  ExportButton,
+  FilterButton,
+  //Field controls
+  BooleanField,
+  DateField,
+  EmailField,
+  ImageField,
+  NumberField,
+  ReferenceField,
+  TextField,
+  UrlField,
+  //Input controls
+  BooleanInput,
+  DateInput,
+  //EmailInput,
+  ImageInput,
+  NumberInput,
+  ReferenceInput,
+  TextInput,
+  //UrlInput,
+} from "react-admin";
+import { useRecordContext } from "react-admin";
+const ListActions = () => (
+    <TopToolbar>
+        <FilterButton />
+        <CreateButton />
+        <ExportButton />
+        <SelectColumnsButton />
+    </TopToolbar>
+);
+const OrderDetailsTitle = () => {
+  const record = useRecordContext();
+  return <span>OrderDetails {record ? `"${ record.orderId }"` : ""}</span>;
+};
+
+export const OrderDetailsList = () => (
+      <List actions={<ListActions  />} filters={ResourceFilters} >
+        <DatagridConfigurable>
+          <ReferenceField source="orderId" reference="Orders"  />
+<ReferenceField source="productId" reference="Products"  />
+<NumberField source="price" />
+<NumberField source="quantity" />
+<NumberField source="id" /><EditButton />
+
+        </DatagridConfigurable>
+      </List>
+      );
+
+export const OrderDetailsEdit = () => (
+                    <Edit title={<OrderDetailsTitle />}>
+                      <SimpleForm>
+                          <ReferenceInput source="orderId"  reference="Orders"   />
+<ReferenceInput source="productId"  reference="Products"   />
+<NumberInput source="price"   />
+<NumberInput source="quantity"   />
+<NumberInput source="id"   disabled/>
+                      </SimpleForm>
+                    </Edit>
+                  );
+
+export const OrderDetailsCreate = () => (
+                                  <Create>
+                                    <SimpleForm>
+                                        <ReferenceInput source="orderId"  reference="Orders"   />
+<ReferenceInput source="productId"  reference="Products"   />
+<NumberInput source="price"   />
+<NumberInput source="quantity"   />
+<NumberInput source="id"   disabled/>
+                                    </SimpleForm>
+                                  </Create>
+                                );
+
+const ResourceFilters = [
+      <TextInput source="q" label="Search" alwaysOn />,
+<ReferenceInput source="orderId" label="orderId" reference="Orders"   alwaysOn/>,
+<ReferenceInput source="productId" label="productId" reference="Products"   alwaysOn/>,
+,
+,
+
+    ];
+
+
