2115fa94904affa1cbb7bfb5cb121470ae81d40a
11ai <124818466+11ai@users.noreply.github.com>
2024-02-06 19:55:50 +0200

    edited on Tue Feb 06 2024 19:55:40 GMT+0200 (Eastern European Standard Time)

--- frontend/src/data.ts
+++ frontend/src/data.ts
@@ -1,3 +1,3 @@
 
-let data: any = {"defaultData":{"books":[]}};
+let data: any = {"defaultData":{"books":[{"title":"glum","isbn":"aha","id":189816465522688},{"title":"adventurously","isbn":"slur","id":731624492761088},{"title":"after","isbn":"applaud","id":8943540126089216}]}};
 export default data;
