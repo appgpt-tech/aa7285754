83bd4679a4ee7659d192559e8f1c3910200216e1
11ai <124818466+11ai@users.noreply.github.com>
2024-02-06 20:15:48 +0200

    edited on Tue Feb 06 2024 20:15:35 GMT+0200 (Eastern European Standard Time)

--- frontend/src/i18nProvider.ts
+++ frontend/src/i18nProvider.ts
@@ -4,8 +4,8 @@
     import enOriginal from 'ra-language-english';
 import frOriginal from 'ra-language-french';
 
-    const enResources = { resources: {"books":{"name":"Books","fields":{"title":"Title","isbn":"Isbn","id":"id"}}}};
-const frResources = { resources: {"books":{"name":"Books (fr)","fields":{"title":"Title (fr)","isbn":"Isbn (fr)","id":"id"}}}};
+    const enResources = { resources: {"Users":{"name":"Users","fields":{"userId":"User Id","email":"Email","name":"Name","id":"id"}},"Cards":{"name":"Cards","fields":{"cardId":"Card Id","setid":"Setid","cardName":"Card Name","serial":"Serial","type":"Type","rarity":"Rarity","condition":"Condition","imageurl":"Imageurl","id":"id"}},"Sets":{"name":"Sets","fields":{"setId":"Set Id","setname":"Setname","releasedate":"Releasedate","totalcards":"Totalcards","id":"id"}},"Inventory":{"name":"Inventory","fields":{"userId":"User Id","cardId":"Card Id","recordedDate":"Recorded Date","id":"id"}},"Wishlist":{"name":"Wishlist","fields":{"userId":"User Id","cardId":"Card Id","insertedDate":"Inserted Date","id":"id"}}}};
+const frResources = { resources: {"Users":{"name":"Users (fr)","fields":{"userId":"User Id (fr)","email":"Email (fr)","name":"Name (fr)","id":"id"}},"Cards":{"name":"Cards (fr)","fields":{"cardId":"Card Id (fr)","setid":"Setid (fr)","cardName":"Card Name (fr)","serial":"Serial (fr)","type":"Type (fr)","rarity":"Rarity (fr)","condition":"Condition (fr)","imageurl":"Imageurl (fr)","id":"id"}},"Sets":{"name":"Sets (fr)","fields":{"setId":"Set Id (fr)","setname":"Setname (fr)","releasedate":"Releasedate (fr)","totalcards":"Totalcards (fr)","id":"id"}},"Inventory":{"name":"Inventory (fr)","fields":{"userId":"User Id (fr)","cardId":"Card Id (fr)","recordedDate":"Recorded Date (fr)","id":"id"}},"Wishlist":{"name":"Wishlist (fr)","fields":{"userId":"User Id (fr)","cardId":"Card Id (fr)","insertedDate":"Inserted Date (fr)","id":"id"}}}};
 
 
     const en = mergeTranslations(enOriginal,enResources);
