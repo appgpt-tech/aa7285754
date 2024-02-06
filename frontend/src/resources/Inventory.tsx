cd8a92b24825300118cd4513a243454bdd0df53b
11ai <124818466+11ai@users.noreply.github.com>
2024-02-07 01:00:36 +0200

    edited on Tue Feb 06 2024 23:00:18 GMT+0000 (Coordinated Universal Time)

--- frontend/src/resources/Inventory.tsx
+++ frontend/src/resources/Inventory.tsx
@@ -41,15 +41,15 @@ const ListActions = () => (
 );
 const InventoryTitle = () => {
   const record = useRecordContext();
-  return <span>Inventory {record ? `"${ record.userId }"` : ""}</span>;
+  return <span>Inventory {record ? `"${ record.productId }"` : ""}</span>;
 };
 
 export const InventoryList = () => (
       <List actions={<ListActions  />} filters={ResourceFilters} >
         <DatagridConfigurable>
-          <ReferenceField source="userId" reference="Users"  />
-<ReferenceField source="cardId" reference="Cards"  />
-<DateField source="recordedDate" />
+          <TextField source="productId" />
+<NumberField source="quantity" />
+<NumberField source="lowStockThreshold" />
 <NumberField source="id" /><EditButton />
 
         </DatagridConfigurable>
@@ -59,9 +59,9 @@ export const InventoryList = () => (
 export const InventoryEdit = () => (
                     <Edit title={<InventoryTitle />}>
                       <SimpleForm>
-                          <ReferenceInput source="userId"  reference="Users"   />
-<ReferenceInput source="cardId"  reference="Cards"   />
-<DateInput source="recordedDate"   />
+                          <TextInput source="productId"   />
+<NumberInput source="quantity"   />
+<NumberInput source="lowStockThreshold"   />
 <NumberInput source="id"   disabled/>
                       </SimpleForm>
                     </Edit>
@@ -70,9 +70,9 @@ export const InventoryEdit = () => (
 export const InventoryCreate = () => (
                                   <Create>
                                     <SimpleForm>
-                                        <ReferenceInput source="userId"  reference="Users"   />
-<ReferenceInput source="cardId"  reference="Cards"   />
-<DateInput source="recordedDate"   />
+                                        <TextInput source="productId"   />
+<NumberInput source="quantity"   />
+<NumberInput source="lowStockThreshold"   />
 <NumberInput source="id"   disabled/>
                                     </SimpleForm>
                                   </Create>
@@ -80,8 +80,8 @@ export const InventoryCreate = () => (
 
 const ResourceFilters = [
       <TextInput source="q" label="Search" alwaysOn />,
-<ReferenceInput source="userId" label="userId" reference="Users"   alwaysOn/>,
-<ReferenceInput source="cardId" label="cardId" reference="Cards"   alwaysOn/>,
+,
+,
 ,
 
     ];
