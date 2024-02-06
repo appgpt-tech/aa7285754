47e7a196b5314fc87cf3e595ddcab4e5a44a46c7
11ai <124818466+11ai@users.noreply.github.com>
2024-02-07 01:00:40 +0200

    created on Tue Feb 06 2024 23:00:18 GMT+0000 (Coordinated Universal Time)

---
+++ frontend/src/resources/ProductCategories.tsx
@@ -0,0 +1,89 @@
+import {
+  Datagrid,
+  List,
+  EditButton,
+  Edit,
+  SimpleForm,
+  Create,
+  SelectColumnsButton,
+  DatagridConfigurable,
+  TopToolbar,
+  CreateButton,
+  ExportButton,
+  FilterButton,
+  //Field controls
+  BooleanField,
+  DateField,
+  EmailField,
+  ImageField,
+  NumberField,
+  ReferenceField,
+  TextField,
+  UrlField,
+  //Input controls
+  BooleanInput,
+  DateInput,
+  //EmailInput,
+  ImageInput,
+  NumberInput,
+  ReferenceInput,
+  TextInput,
+  //UrlInput,
+} from "react-admin";
+import { useRecordContext } from "react-admin";
+const ListActions = () => (
+    <TopToolbar>
+        <FilterButton />
+        <CreateButton />
+        <ExportButton />
+        <SelectColumnsButton />
+    </TopToolbar>
+);
+const ProductCategoriesTitle = () => {
+  const record = useRecordContext();
+  return <span>ProductCategories {record ? `"${ record.categoryId }"` : ""}</span>;
+};
+
+export const ProductCategoriesList = () => (
+      <List actions={<ListActions  />} filters={ResourceFilters} >
+        <DatagridConfigurable>
+          <TextField source="categoryId" />
+<ReferenceField source="productId" reference="Products"  />
+<TextField source="description" />
+<NumberField source="id" /><EditButton />
+
+        </DatagridConfigurable>
+      </List>
+      );
+
+export const ProductCategoriesEdit = () => (
+                    <Edit title={<ProductCategoriesTitle />}>
+                      <SimpleForm>
+                          <TextInput source="categoryId"   />
+<ReferenceInput source="productId"  reference="Products"   />
+<TextInput source="description"   />
+<NumberInput source="id"   disabled/>
+                      </SimpleForm>
+                    </Edit>
+                  );
+
+export const ProductCategoriesCreate = () => (
+                                  <Create>
+                                    <SimpleForm>
+                                        <TextInput source="categoryId"   />
+<ReferenceInput source="productId"  reference="Products"   />
+<TextInput source="description"   />
+<NumberInput source="id"   disabled/>
+                                    </SimpleForm>
+                                  </Create>
+                                );
+
+const ResourceFilters = [
+      <TextInput source="q" label="Search" alwaysOn />,
+,
+<ReferenceInput source="productId" label="productId" reference="Products"   alwaysOn/>,
+,
+
+    ];
+
+
