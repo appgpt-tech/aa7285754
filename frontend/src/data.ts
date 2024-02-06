cdb318abd87be646d9e51340aa35197db76cda94
11ai <124818466+11ai@users.noreply.github.com>
2024-02-06 19:58:01 +0200

    edited on Tue Feb 06 2024 19:57:50 GMT+0200 (Eastern European Standard Time)

--- frontend/src/data.ts
+++ frontend/src/data.ts
@@ -1,3 +1,3 @@
 
-let data: any = {"defaultData":{"books":[{"title":"glum","isbn":"aha","id":189816465522688},{"title":"adventurously","isbn":"slur","id":731624492761088},{"title":"after","isbn":"applaud","id":8943540126089216}]}};
+let data: any = {"defaultData":{"books":[{"title":"under","isbn":"advanced","id":80933583585280},{"title":"partake","isbn":"lovable","id":6769201614159872},{"title":"despite","isbn":"worth","id":7804631990140928},{"title":"birth","isbn":"impish","id":8564559038119936},{"title":"vague","isbn":"opposite","id":4456792119574528}]}};
 export default data;
