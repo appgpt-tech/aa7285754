e98c3534435a1a8a65e479811a680e0db6e08626
11ai <124818466+11ai@users.noreply.github.com>
2024-02-07 14:20:03 +0200

    edited on Wed Feb 07 2024 14:19:55 GMT+0200 (Eastern European Standard Time)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,16 +8,10 @@ import { authProvider, apInitialize } from "./authProvider";
 import { i18nProvider } from "./i18nProvider";
 import LoginPage, { Login } from "./Login";
 import data from "./data";
-import { UsersList, UsersCreate, UsersEdit} from "./resources/Users";
-import { EmployeesList, EmployeesCreate, EmployeesEdit} from "./resources/Employees";
-import { EvaluationsList, EvaluationsCreate, EvaluationsEdit} from "./resources/Evaluations";
-import { PayrollList, PayrollCreate, PayrollEdit} from "./resources/Payroll";
-import { LeaveRequestsList, LeaveRequestsCreate, LeaveRequestsEdit} from "./resources/LeaveRequests";
-import UsersIcon from "@mui/icons-material/Person";
-import EmployeesIcon from "@mui/icons-material/Work";
-import EvaluationsIcon from "@mui/icons-material/Assessment";
-import PayrollIcon from "@mui/icons-material/Payments";
-import LeaveRequestsIcon from "@mui/icons-material/BeachAccess"; 
+import { booksList, booksCreate, booksEdit} from "./resources/books";
+import { authorsList, authorsCreate, authorsEdit} from "./resources/authors";
+import booksIcon from "@mui/icons-material/MenuBook";
+import authorsIcon from "@mui/icons-material/Person"; 
 // SUPERTOKENS
 import React from "react";
 import SuperTokens, {
@@ -79,36 +73,18 @@ const App = () => (
         dashboard={Dashboard}
         
       >
-    <Resource name="Users" options={{label:"Users"}} 
-list={UsersList}
-create={UsersCreate}
-edit={UsersEdit}
-recordRepresentation="userId"
-icon={UsersIcon}/>
-<Resource name="Employees" options={{label:"Employees"}} 
-list={EmployeesList}
-create={EmployeesCreate}
-edit={EmployeesEdit}
-recordRepresentation="employeeId"
-icon={EmployeesIcon}/>
-<Resource name="Evaluations" options={{label:"Evaluations"}} 
-list={EvaluationsList}
-create={EvaluationsCreate}
-edit={EvaluationsEdit}
-recordRepresentation="reviewId"
-icon={EvaluationsIcon}/>
-<Resource name="Payroll" options={{label:"Payroll"}} 
-list={PayrollList}
-create={PayrollCreate}
-edit={PayrollEdit}
-recordRepresentation="payrollId"
-icon={PayrollIcon}/>
-<Resource name="LeaveRequests" options={{label:"Leave Requests"}} 
-list={LeaveRequestsList}
-create={LeaveRequestsCreate}
-edit={LeaveRequestsEdit}
-recordRepresentation="requestId"
-icon={LeaveRequestsIcon}/>
+    <Resource name="books" options={{label:"Books"}} 
+list={booksList}
+create={booksCreate}
+edit={booksEdit}
+recordRepresentation="title"
+icon={booksIcon}/>
+<Resource name="authors" options={{label:"Authors"}} 
+list={authorsList}
+create={authorsCreate}
+edit={authorsEdit}
+recordRepresentation="authorName"
+icon={authorsIcon}/>
     <CustomRoutes noLayout>
       {/*This renders the login UI on the /auth route*/}
       {getSuperTokensRoutesForReactRouterDom(reactRouterDom)}
