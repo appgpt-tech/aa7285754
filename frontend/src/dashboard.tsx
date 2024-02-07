20d498faa28da0f7779bb5fcb1913d6c65236452
11ai <124818466+11ai@users.noreply.github.com>
2024-02-07 20:48:03 +0200

    edited on Wed Feb 07 2024 18:47:48 GMT+0000 (Coordinated Universal Time)

--- frontend/src/dashboard.tsx
+++ frontend/src/dashboard.tsx
@@ -1,7 +1,7 @@
 import { Card, CardContent, CardHeader } from "@mui/material";
     export const Dashboard = () => (
       <Card>
-      <CardHeader title= "libby" />
-      <CardContent> music colln < /CardContent>
+      <CardHeader title= "EmpowerHR" />
+      <CardContent> My app is an HR application used by a company to streamline the management of employee information, recruitment, performance evaluations, leave requests, and payroll. < /CardContent>
       </Card>
     );
