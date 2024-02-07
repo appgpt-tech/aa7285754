7086d5dcce528351b49167b1916e6aa1368b3b22
11ai <124818466+11ai@users.noreply.github.com>
2024-02-07 17:15:50 +0200

    created on Wed Feb 07 2024 17:15:34 GMT+0200 (Eastern European Standard Time)

---
+++ frontend/src/resources/artists.tsx
@@ -0,0 +1,89 @@
+import {
+  Datagrid,
+  List,
+  EditButton,
+  Edit,
+  SimpleForm,
+  Create,
+  SelectColumnsButton,
+  DatagridConfigurable,
+  TopToolbar,
+  CreateButton,
+  ExportButton,
+  FilterButton,
+  //Field controls
+  BooleanField,
+  DateField,
+  EmailField,
+  ImageField,
+  NumberField,
+  ReferenceField,
+  TextField,
+  UrlField,
+  //Input controls
+  BooleanInput,
+  DateInput,
+  //EmailInput,
+  ImageInput,
+  NumberInput,
+  ReferenceInput,
+  TextInput,
+  //UrlInput,
+} from "react-admin";
+import { useRecordContext } from "react-admin";
+const ListActions = () => (
+    <TopToolbar>
+        <FilterButton />
+        <CreateButton />
+        <ExportButton />
+        <SelectColumnsButton />
+    </TopToolbar>
+);
+const artistsTitle = () => {
+  const record = useRecordContext();
+  return <span>artists {record ? `"${ record.artistName }"` : ""}</span>;
+};
+
+export const artistsList = () => (
+      <List actions={<ListActions  />} filters={ResourceFilters} >
+        <DatagridConfigurable>
+          <TextField source="artistName" />
+<TextField source="band" />
+<DateField source="dateOfBirth" />
+<NumberField source="id" /><EditButton />
+
+        </DatagridConfigurable>
+      </List>
+      );
+
+export const artistsEdit = () => (
+                    <Edit title={<artistsTitle />}>
+                      <SimpleForm>
+                          <TextInput source="artistName"   />
+<TextInput source="band"   />
+<DateInput source="dateOfBirth"   />
+<NumberInput source="id"   disabled/>
+                      </SimpleForm>
+                    </Edit>
+                  );
+
+export const artistsCreate = () => (
+                                  <Create>
+                                    <SimpleForm>
+                                        <TextInput source="artistName"   />
+<TextInput source="band"   />
+<DateInput source="dateOfBirth"   />
+<NumberInput source="id"   disabled/>
+                                    </SimpleForm>
+                                  </Create>
+                                );
+
+const ResourceFilters = [
+      <TextInput source="q" label="Search" alwaysOn />,
+,
+,
+,
+
+    ];
+
+
