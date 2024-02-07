f665a62170c22c154cb6d8c6d2a78bb539794c62
11ai <124818466+11ai@users.noreply.github.com>
2024-02-07 14:20:45 +0200

    edited on Wed Feb 07 2024 14:20:29 GMT+0200 (Eastern European Standard Time)

--- frontend/src/i18nProvider.ts
+++ frontend/src/i18nProvider.ts
@@ -4,8 +4,8 @@
     import enOriginal from 'ra-language-english';
 import frOriginal from 'ra-language-french';
 
-    const enResources = { resources: {"books":{"name":"Books","fields":{"title":"Title","isbn":"Isbn","id":"id"}},"authors":{"name":"Authors","fields":{"authorName":"Author Name","gender":"Gender","dateOfBirth":"Date Of Birth","numberOfBooks":"Number Of Books","id":"id"}}}};
-const frResources = { resources: {"books":{"name":"Books (fr)","fields":{"title":"Title (fr)","isbn":"Isbn (fr)","id":"id"}},"authors":{"name":"Authors (fr)","fields":{"authorName":"Author Name (fr)","gender":"Gender (fr)","dateOfBirth":"Date Of Birth (fr)","numberOfBooks":"Number Of Books (fr)","id":"id"}}}};
+    const enResources = { resources: {"books":{"name":"Books","fields":{"title":"Title","isbn":"Isbn","author":"Author","id":"id"}},"authors":{"name":"Authors","fields":{"authorName":"Author Name","gender":"Gender","dateOfBirth":"Date Of Birth","numberOfBooks":"Number Of Books","id":"id"}}}};
+const frResources = { resources: {"books":{"name":"Books (fr)","fields":{"title":"Title (fr)","isbn":"Isbn (fr)","author":"Author (fr)","id":"id"}},"authors":{"name":"Authors (fr)","fields":{"authorName":"Author Name (fr)","gender":"Gender (fr)","dateOfBirth":"Date Of Birth (fr)","numberOfBooks":"Number Of Books (fr)","id":"id"}}}};
 
 
     const en = mergeTranslations(enOriginal,enResources);
