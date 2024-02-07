a31d76b2a54298535a841298a2bbabc1954232a6
11ai <124818466+11ai@users.noreply.github.com>
2024-02-07 23:06:24 +0200

    created on Wed Feb 07 2024 21:06:09 GMT+0000 (Coordinated Universal Time)

---
+++ frontend/src/resources/contacts.tsx
@@ -0,0 +1,100 @@
+import {
+  Datagrid,
+  List,
+  EditButton,
+  Edit,
+  SimpleForm,
+  Create,
+  SelectColumnsButton,
+  DatagridConfigurable,
+  TopToolbar,
+  CreateButton,
+  ExportButton,
+  FilterButton,
+  //Field controls
+  BooleanField,
+  DateField,
+  EmailField,
+  ImageField,
+  NumberField,
+  ReferenceField,
+  TextField,
+  UrlField,
+  //Input controls
+  BooleanInput,
+  DateInput,
+  EmailInput,
+  ImageInput,
+  NumberInput,
+  ReferenceInput,
+  TextInput,
+  UrlInput,
+  PasswordInput
+} from "react-admin";
+import { useRecordContext } from "react-admin";
+const ReadOnlyPasswordField = ({ record, source }) => {
+
+  // You can customize the way you display the password here, e.g., mask it with asterisks
+  const maskedPassword =  '********';
+
+  return (
+      <span>{maskedPassword}</span>
+  );
+};
+const ListActions = () => (
+    <TopToolbar>
+        <FilterButton />
+        <CreateButton />
+        <ExportButton />
+        <SelectColumnsButton />
+    </TopToolbar>
+);
+const contactsTitle = () => {
+  const record = useRecordContext();
+  return <span>contacts {record ? `"${ record.name }"` : ""}</span>;
+};
+
+export const contactsList = () => (
+      <List actions={<ListActions  />} filters={ResourceFilters} >
+        <DatagridConfigurable>
+          <TextField source="name" />
+<EmailField source="email" />
+<TextField source="mobileNumber" />
+<UrlField source="linkedinUrl" /><EditButton />
+
+        </DatagridConfigurable>
+      </List>
+      );
+
+export const contactsEdit = () => (
+                    <Edit title={<contactsTitle />}>
+                      <SimpleForm>
+                          <TextInput source="name"   />
+<TextInput source="email"   />
+<TextInput source="mobileNumber"   />
+<TextInput source="linkedinUrl"   />
+                      </SimpleForm>
+                    </Edit>
+                  );
+
+export const contactsCreate = () => (
+                                  <Create>
+                                    <SimpleForm>
+                                        <TextInput source="name"   />
+<TextInput source="email"   />
+<TextInput source="mobileNumber"   />
+<TextInput source="linkedinUrl"   />
+                                    </SimpleForm>
+                                  </Create>
+                                );
+
+const ResourceFilters = [
+      <TextInput source="q" label="Search" alwaysOn />,
+,
+,
+,
+,
+
+    ];
+
+
