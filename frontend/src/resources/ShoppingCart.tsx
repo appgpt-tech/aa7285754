70b56194b50926b672263dfc1461ed3c18511aac
11ai <124818466+11ai@users.noreply.github.com>
2024-02-07 21:14:49 +0200

    edited on Wed Feb 07 2024 21:14:27 GMT+0200 (Eastern European Standard Time)

--- frontend/src/resources/ShoppingCart.tsx
+++ frontend/src/resources/ShoppingCart.tsx
@@ -51,8 +51,7 @@ export const ShoppingCartList = () => (
 <ReferenceField source="customerId" reference="Customers"  />
 <ReferenceField source="productId" reference="Products"  />
 <NumberField source="price" />
-<NumberField source="quantity" />
-<NumberField source="id" /><EditButton />
+<NumberField source="quantity" /><EditButton />
 
         </DatagridConfigurable>
       </List>
@@ -66,7 +65,6 @@ export const ShoppingCartEdit = () => (
 <ReferenceInput source="productId"  reference="Products"   />
 <NumberInput source="price"   />
 <NumberInput source="quantity"   />
-<NumberInput source="id"   disabled/>
                       </SimpleForm>
                     </Edit>
                   );
@@ -79,7 +77,6 @@ export const ShoppingCartCreate = () => (
 <ReferenceInput source="productId"  reference="Products"   />
 <NumberInput source="price"   />
 <NumberInput source="quantity"   />
-<NumberInput source="id"   disabled/>
                                     </SimpleForm>
                                   </Create>
                                 );
