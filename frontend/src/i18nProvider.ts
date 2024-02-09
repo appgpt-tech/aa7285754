b55df7f1b37be71fee73c970cac715e5713d156f
11ai <124818466+11ai@users.noreply.github.com>
2024-02-09 23:19:55 +0200

    edited on Fri Feb 09 2024 23:19:44 GMT+0200 (Eastern European Standard Time)

--- frontend/src/i18nProvider.ts
+++ frontend/src/i18nProvider.ts
@@ -4,8 +4,8 @@
     import enOriginal from 'ra-language-english';
 import frOriginal from 'ra-language-french';
 
-    const enResources = { resources: {"friends":{"name":"Friends","fields":{"name":"Name","birthday":"Birthday","starSign":"Star Sign","gender":"Gender","email":"Email","id":"id"}},"websites":{"name":"Websites","fields":{"siteName":"Site Name","url":"Url","rating":"Rating","id":"id"}},"credentials":{"name":"Credentials","fields":{"url":"Url","username":"Username","password":"Password","id":"id"}}}};
-const frResources = { resources: {"friends":{"name":"Friends (fr)","fields":{"name":"Name (fr)","birthday":"Birthday (fr)","starSign":"Star Sign (fr)","gender":"Gender (fr)","email":"Email (fr)","id":"id"}},"websites":{"name":"Websites (fr)","fields":{"siteName":"Site Name (fr)","url":"Url (fr)","rating":"Rating (fr)","id":"id"}},"credentials":{"name":"Credentials (fr)","fields":{"url":"Url (fr)","username":"Username (fr)","password":"Password (fr)","id":"id"}}}};
+    const enResources = { resources: {"contacts":{"name":"Contacts","fields":{"name":"Name","mobile":"Mobile","email":"Email","id":"id"}},"tasks":{"name":"Tasks","fields":{"taskTitle":"Task Title","description":"Description","scheduledStartDate":"Scheduled Start Date","scheduledEndDate":"Scheduled End Date","actualEndDate":"Actual End Date","id":"id"}},"credentials":{"name":"Credentials","fields":{"url":"Url","username":"Username","password":"Password","id":"id"}}}};
+const frResources = { resources: {"contacts":{"name":"Contacts (fr)","fields":{"name":"Name (fr)","mobile":"Mobile (fr)","email":"Email (fr)","id":"id"}},"tasks":{"name":"Tasks (fr)","fields":{"taskTitle":"Task Title (fr)","description":"Description (fr)","scheduledStartDate":"Scheduled Start Date (fr)","scheduledEndDate":"Scheduled End Date (fr)","actualEndDate":"Actual End Date (fr)","id":"id"}},"credentials":{"name":"Credentials (fr)","fields":{"url":"Url (fr)","username":"Username (fr)","password":"Password (fr)","id":"id"}}}};
 
 
     const en = mergeTranslations(enOriginal,enResources);
