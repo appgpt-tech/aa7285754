bb147226bb78c4550eeb90be31c0c3024a0742bc
11ai <124818466+11ai@users.noreply.github.com>
2024-02-09 23:24:14 +0200

    edited on Fri Feb 09 2024 23:23:39 GMT+0200 (Eastern European Standard Time)

--- backend/src/Database.ts
+++ backend/src/Database.ts
@@ -3,9 +3,11 @@ import { DataSource } from "typeorm";
 import { DBConfiguration } from "./Configuration";
 import { SettingsEntity } from "./db/Settings.entity";
 //autogenerate imports based on resources
-import { contactsEntity } from "./db/contacts.entity";
-import { tasksEntity } from "./db/tasks.entity";
-import { credentialsEntity } from "./db/credentials.entity";
+import { UsersEntity } from "./db/Users.entity";
+import { CardsEntity } from "./db/Cards.entity";
+import { SetsEntity } from "./db/Sets.entity";
+import { InventoryEntity } from "./db/Inventory.entity";
+import { WishlistEntity } from "./db/Wishlist.entity";
 
 export class Database {
   static dbConfiguration: DBConfiguration;
@@ -16,7 +18,7 @@ export class Database {
     let dbConfig: any = dbConfiguration as any;
     //Autogenerate entities array from resource names
 
-    dbConfig.entities = [SettingsEntity, contactsEntity, tasksEntity, credentialsEntity];
+    dbConfig.entities = [SettingsEntity, UsersEntity, CardsEntity, SetsEntity, InventoryEntity, WishlistEntity];
     Database.ds = new DataSource(dbConfig);
     await Database.ds.initialize();
 
@@ -26,16 +28,18 @@ export class Database {
     await Database.Seed();
   }
   static async Seed() {
-    let data: any = {"contacts":[{"name":"name 1","mobile":"mobile 1","email":"email 1","id":22},{"name":"name 2","mobile":"mobile 2","email":"email 2","id":17},{"name":"name 3","mobile":"mobile 3","email":"email 3","id":2},{"name":"name 4","mobile":"mobile 4","email":"email 4","id":16},{"name":"name 5","mobile":"mobile 5","email":"email 5","id":59}],"tasks":[{"taskTitle":"taskTitle 1","description":"description 1","scheduledStartDate":"2023-10-23T13:19:45.816Z","scheduledEndDate":"2024-03-19T06:16:21.691Z","actualEndDate":"2024-09-21T20:41:15.305Z","id":17},{"taskTitle":"taskTitle 2","description":"description 2","scheduledStartDate":"2024-08-08T13:05:29.415Z","scheduledEndDate":"2023-06-20T09:56:40.630Z","actualEndDate":"2024-02-15T19:58:22.856Z","id":72},{"taskTitle":"taskTitle 3","description":"description 3","scheduledStartDate":"2024-05-05T23:58:56.093Z","scheduledEndDate":"2023-06-24T04:54:52.641Z","actualEndDate":"2023-06-06T00:04:35.784Z","id":22},{"taskTitle":"taskTitle 4","description":"description 4","scheduledStartDate":"2025-01-09T14:08:28.772Z","scheduledEndDate":"2024-10-24T04:27:50.353Z","actualEndDate":"2024-06-26T18:02:01.953Z","id":73},{"taskTitle":"taskTitle 5","description":"description 5","scheduledStartDate":"2023-12-16T22:25:10.688Z","scheduledEndDate":"2024-04-16T07:34:31.690Z","actualEndDate":"2024-02-21T22:11:05.524Z","id":20}],"credentials":[{"url":"url 1","username":"username 1","password":"password 1","id":21},{"url":"url 2","username":"username 2","password":"password 2","id":28},{"url":"url 3","username":"username 3","password":"password 3","id":67},{"url":"url 4","username":"username 4","password":"password 4","id":17},{"url":"url 5","username":"username 5","password":"password 5","id":71}]};
+    let data: any = {"Users":[{"userId":"userId 1","email":"email 1","name":"name 1","id":53},{"userId":"userId 2","email":"email 2","name":"name 2","id":85},{"userId":"userId 3","email":"email 3","name":"name 3","id":38},{"userId":"userId 4","email":"email 4","name":"name 4","id":32},{"userId":"userId 5","email":"email 5","name":"name 5","id":35}],"Cards":[{"cardId":"cardId 1","setid":1,"cardName":"cardName 1","serial":"serial 1","type":"type 1","rarity":"rarity 1","condition":"condition 1","imageurl":"imageurl 1","id":37},{"cardId":"cardId 2","setid":2,"cardName":"cardName 2","serial":"serial 2","type":"type 2","rarity":"rarity 2","condition":"condition 2","imageurl":"imageurl 2","id":64},{"cardId":"cardId 3","setid":3,"cardName":"cardName 3","serial":"serial 3","type":"type 3","rarity":"rarity 3","condition":"condition 3","imageurl":"imageurl 3","id":96},{"cardId":"cardId 4","setid":4,"cardName":"cardName 4","serial":"serial 4","type":"type 4","rarity":"rarity 4","condition":"condition 4","imageurl":"imageurl 4","id":38},{"cardId":"cardId 5","setid":5,"cardName":"cardName 5","serial":"serial 5","type":"type 5","rarity":"rarity 5","condition":"condition 5","imageurl":"imageurl 5","id":92}],"Sets":[{"setId":"setId 1","setname":"setname 1","releasedate":"2023-08-04T01:18:54.948Z","totalcards":1,"id":78},{"setId":"setId 2","setname":"setname 2","releasedate":"2024-01-22T19:22:18.838Z","totalcards":2,"id":44},{"setId":"setId 3","setname":"setname 3","releasedate":"2024-06-21T10:51:03.623Z","totalcards":3,"id":24},{"setId":"setId 4","setname":"setname 4","releasedate":"2023-11-18T11:22:22.937Z","totalcards":4,"id":72},{"setId":"setId 5","setname":"setname 5","releasedate":"2024-11-01T07:54:40.528Z","totalcards":5,"id":49}],"Inventory":[{"userId":1,"cardId":1,"recordedDate":"2023-10-27T15:59:30.114Z","id":3},{"userId":2,"cardId":2,"recordedDate":"2023-11-22T17:59:59.323Z","id":15},{"userId":3,"cardId":3,"recordedDate":"2023-09-21T05:41:40.066Z","id":79},{"userId":4,"cardId":4,"recordedDate":"2024-11-23T23:53:46.852Z","id":17},{"userId":5,"cardId":5,"recordedDate":"2023-11-26T23:45:09.779Z","id":66}],"Wishlist":[{"userId":1,"cardId":1,"insertedDate":"2023-09-09T18:52:07.431Z","id":26},{"userId":2,"cardId":2,"insertedDate":"2024-02-14T01:07:24.813Z","id":62},{"userId":3,"cardId":3,"insertedDate":"2024-02-19T00:33:33.450Z","id":88},{"userId":4,"cardId":4,"insertedDate":"2024-03-19T14:34:28.843Z","id":46},{"userId":5,"cardId":5,"insertedDate":"2023-10-17T05:36:45.235Z","id":95}]};
     //Autogenerate multiple such calls ie for each resource and its data object
     let isSeeded = await this.IsSeeded();
     //if (!isSeeded) {
     //forcing app recreation
     if (true){
       console.log('   Seeding database...');
-      await this.SeedResource("contactsEntity", data.contacts);
-await this.SeedResource("tasksEntity", data.tasks);
-await this.SeedResource("credentialsEntity", data.credentials); 
+      await this.SeedResource("UsersEntity", data.Users);
+await this.SeedResource("CardsEntity", data.Cards);
+await this.SeedResource("SetsEntity", data.Sets);
+await this.SeedResource("InventoryEntity", data.Inventory);
+await this.SeedResource("WishlistEntity", data.Wishlist); 
       await this.SeedResource("SettingsEntity", {
         settingname: "isSeeded",
         settingvalue: "true",
